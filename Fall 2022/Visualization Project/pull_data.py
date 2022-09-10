ff5ae5a204acbdd708e82777bcd6f2e2912cd8e6
Caleb Hong <chong02@berkeley.edu>
2022-09-09 17:57:31 -0700

    add data pulling script

---
+++ Fall 2022/Visualization Project/pull_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+
+url = 'https://data.chhs.ca.gov/api/3/action/datastore_search_sql?sql=SELECT%20*%20from%20%22641c5557-7d65-4379-8fea-6b7dedbda40b%22'
+request = requests.get(url)
+json_object = request.json()
+
+data_file = pd.DataFrame(json_object['result']['records'])
+data_file.to_csv('ca_healthcare_facilities.csv', index=False)
